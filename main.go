22ce7a9a175fbbe4c1031678ac8639b6952098ec
Nicolas Morales <nicomorales297@gmail.com>
2023-11-22 23:47:43 -0300

    add PORT

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/nmorales1991/first-api-go/api/handlers"
 	"github.com/nmorales1991/first-api-go/pkg/repository"
 	"github.com/nmorales1991/first-api-go/pkg/services"
+	"os"
 )
 
 func main() {
@@ -17,7 +19,14 @@ func main() {
 	r.POST("/users", userHandler.CreateUser)
 	r.PUT("/users/:id", userHandler.UpdateUser)
 
-	err := r.Run()
+	if _, err := os.Stat(".env"); err == nil {
+		if err := godotenv.Load(); err != nil {
+			panic("Error loading .env file")
+		}
+	}
+	uri := os.Getenv("PORT")
+
+	err := r.Run(":" + uri)
 	if err != nil {
 		return
 	}
